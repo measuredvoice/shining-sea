68b03838b263878a4c9f3c9df0e1fafedef36881
Chris Radcliff <chris.radcliff@gmail.com>
2013-08-20 13:58:34 -0700

    Added simple date pagination. (Only shows previous date on the main index page.)

--- site/lib/helpers/link.rb
+++ site/lib/helpers/link.rb
@@ -3,4 +3,16 @@ module LinkHelper
     return '' unless tweet
     "/#{tweet[:screen_name]}/status/#{tweet[:tweet_id]}/"
   end
+  
+  def date_link(date_str, direction)
+    start_date = Time.zone.parse(date_str)
+    case direction
+    when :previous, :prev
+      link_date = start_date - 1.day
+    when :next
+      link_date = start_date + 1.day
+    end
+    
+    "<a href=\"/top50/#{link_date.strftime('%Y-%m-%d')}\">#{link_date.strftime('%B %-d, %Y')}</a>"
+  end
 end
